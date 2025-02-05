d60e0435d42a10389884199e8a97cdec291a5e98
Ruben Aldair <ruben.aldair.gastelum@cetmar11.edu.mx>
2025-02-05 00:04:30 -0800

    Rising bring up

--- rom/lineage.mk
+++ rom/lineage.mk
@@ -24,6 +24,28 @@ $(call inherit-product, device/motorola/targets/include/kernel/source.mk)
 # Libhidl
 PLATFORM_PROVIDES_LIBHIDL := true
 
+## risingOS-Flags
+# Ship Basic Call Recorder
+TARGET_PREBUILT_BCR := true
+# Addons
+TARGET_HAS_UDFPS := true
+# Gms 
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+# Ship Pixel Launcher
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+# Lawnchair Prebuilt
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+# rising Maintainer
+RISING_MAINTAINER=AldairSoraki
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingMaintainer="AldairSoraki" \
+    RisingChipset="SM6125"
+
 # Required Scripts
 $(warning This ROM requires replace_camera_sepolicy.sh to be ran...)
 
